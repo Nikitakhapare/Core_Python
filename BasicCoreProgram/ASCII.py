35e605c2c3d7b5d5ce9bb123f7c5f085bd8f4c18
Nikita <nikitakhapare53@gmail.com>
2022-04-08 17:01:37 +0530

    [nikita]Add: Python program to find ASCII value of Character

---
+++ BasicCoreProgram/ASCII.py
@@ -0,0 +1,12 @@
+'''
+@Author: Nikita
+@Date: 2022-04-04 20: 08: 00
+@Last Modified by: Nikita
+@Last Modified time: 2022-04-04 20:08:00
+@Title: ASCII value of Character
+'''
+#  ord() return Unicode code point of that character.
+if __name__== '__main__':
+    char=input("enter a charater")
+    print("The ASCII value of '" , char , "' is", ord(char))
+
